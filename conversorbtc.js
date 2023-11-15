901e04fb2a68f4390512f99ac9a2b1cee9f2ee2c
mateuscamargofernandes <128862400+teuzsw@users.noreply.github.com>
2023-11-15 16:06:05 -0300

    Create conversorbtc.js

---
+++ conversorbtc.js
@@ -0,0 +1,14 @@
+let btcBr = 181021.12;
+let btcEu = 34489.28;
+let btcDo = 37404.80;
+let real = 1;
+let dolar =1;
+let euro = 1;
+
+let btcReal = btcBr * real;
+let btcEuro = btcEu * euro;
+let btcDolar = btcDo * dolar;
+
+alert('Hoje 1 bitcoin em euro é ' + btcEuro + 'BTC');
+alert('hoje 1 bitcoin em dólar é ' + btcDolar + 'BTC');
+alert('hoje 1 bitcoin em real é ' + btcReal + 'BTC');
